17e6f06fe288feb89154bd519bb5b44762972137
Brett Logan <lindluni@github.com>
2022-05-25 11:21:56 -0400

    Add app authentication

--- index.js
+++ index.js
@@ -4,34 +4,64 @@ const yaml = require('js-yaml')
 
 const core = require('@actions/core')
 const {Octokit} = require('@octokit/rest')
+const {App} = require('@octokit/app')
 const {retry} = require('@octokit/plugin-retry')
 const {throttling} = require('@octokit/plugin-throttling')
 const _Octokit = Octokit.plugin(retry, throttling)
 
+const appID = core.getInput('app-id', {required: false, trimWhitespace: true})
+const privateKey = core.getInput('private-key', {required: false, trimWhitespace: true})
 const baseURL = core.getInput('url', {required: true, trimWhitespace: true})
 const groups = core.getInput('groups', {required: true, trimWhitespace: true}).split('\n').map(group => group.trim())
 const org = core.getInput('org', {required: true, trimWhitespace: true})
 const repo = core.getInput('repo', {required: true, trimWhitespace: true})
-const token = core.getInput('token', {required: true, trimWhitespace: true})
+const token = core.getInput('token', {required: false, trimWhitespace: true})
 
-const client = new _Octokit({
-    auth: token,
-    baseUrl: baseURL,
-    throttle: {
-        onRateLimit: (retryAfter, options, octokit) => {
-            octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`)
-            if (options.request.retryCount <= 1) {
-                octokit.log.info(`Retrying after ${retryAfter} seconds!`)
-                return true
+async function newClient() {
+    return new _Octokit({
+        auth: token,
+        baseUrl: baseURL,
+        throttle: {
+            onRateLimit: (retryAfter, options, octokit) => {
+                octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`)
+                if (options.request.retryCount <= 1) {
+                    octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+                    return true
+                }
+            },
+            onAbuseLimit: (retryAfter, options, octokit) => {
+                octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`)
+            },
+        }
+    })
+}
+
+async function newAppClient() {
+    return new App({
+        appId: appID,
+        privateKey: privateKey,
+        Octokit: _Octokit.defaults({
+            request: {
+                retries: 3
+            },
+            baseUrl: baseURL,
+            throttle: {
+                onRateLimit: (retryAfter, options, octokit) => {
+                    octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+                    if (options.request.retryCount <= 1) {
+                        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+                        return true;
+                    }
+                },
+                onAbuseLimit: (retryAfter, options, octokit) => {
+                    octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`);
+                }
             }
-        },
-        onAbuseLimit: (retryAfter, options, octokit) => {
-            octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`)
-        },
-    }
-})
+        })
+    });
+}
 
-async function retrieveFiles(group, ref) {
+async function retrieveFiles(client, group, ref) {
     try {
         core.info(`Retrieving files for group ${group}`)
         if (ref) {
@@ -55,7 +85,7 @@ async function retrieveFiles(group, ref) {
     }
 }
 
-async function retrieveFile(path, ref) {
+async function retrieveFile(client, path, ref) {
     try {
         core.info(`Retrieving file ${path}`)
         if (ref) {
@@ -96,16 +126,23 @@ async function processVariables(rawContent) {
 
 async function main() {
     try {
+        let client
+        if (appID && privateKey) {
+            client = await newAppClient()
+        } else {
+            client = newClient()
+        }
+        
         for (let group of groups) {
             core.info(`Processing group ${group}`)
             let ref
             if (group.includes('@')) {
                 [group, ref] = group.split('@')
             }
-            const files = await retrieveFiles(group, ref)
+            const files = await retrieveFiles(client, group, ref)
             if (Array.isArray(files)) {
                 for (const _file of files) {
-                    const file = await retrieveFile(_file.path)
+                    const file = await retrieveFile(client, _file.path)
                     await processVariables(file)
                 }
             } else {
